80878223be4c25ac52c383ad265ef0b66919648e
mingyu.ding <15311449479@163.com>
2021-02-19 17:23:26 +0800

    Initial commit

---
+++ 05-元组结合count和index方法的使用.py
@@ -0,0 +1,11 @@
+
+
+# 练习1: 定义一个元组类型的变量, 统计 马忠 在元组中的出现的次数
+result = name_tuple.count("马忠")
+print(result)
+result = name_tuple.count("张辽")
+print(result)
+
+# 练习1: 定义一个元组类型的变量, 获取 关羽 在元组中的下标
+index = name_tuple.index("关羽")
+print(index)
